3ef04f0499c83fbcf568756fffb4a27d43ccad03
Moody Rahman <mrahman01@stuy.edu>
2020-03-06 13:07:19 -0500

    rm matrix stuffos

--- Matrix.java
+++ Matrix.java
@@ -88,53 +88,6 @@ public String toString(){
         return out;
 }
 
-/**
- * generates a 4 by 4 identity Matrix
- * @return identity matrix
- */
-public static Matrix identity(){
-        int[][] out = new int[4][4];
-        for (int x = 0; x < 4; x++) {
-                for (int y = 0; y < 4; y++) {
-                        if (x == y) {
-                                out[x][y] = 1;
-                        }
-                }
-        }
-
-        return new Matrix(out);
-}
-
-
-public static Matrix scale(int sx, int sy, int sz){
-        int[][] out = { {sx, 0, 0, 0},
-                        {0, sy, 0, 0},
-                        {0, 0, sz, 0},
-                        {0, 0, 0,  1}};
-
-        return new Matrix(out);
-}
-
-public static Matrix translate(int dx, int dy, int dz){
-        int[][] out = { {1, 0, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 1, 0},
-                        {dx, dy, dz,  1}};
-        return new Matrix(out);
-}
-
-public static Matrix rotate(char axis, int theta){
-        switch (axis) {
-        case 'x':
-                int[][] out = { {1, 0, 0, 0},
-                                {0, 1, 0, 0},
-                                {0, 0, 1, 0},
-                                {0, 0, 0,  1}};
-                return new Matrix(out);
-        }
-
-        return new Matrix();
-}
 
 /**
  * converts the internal array for the matrix into an int[][]
@@ -219,7 +172,7 @@ public Image flushToImage(){
 
 public static void main(String[] args) {
 
-        Matrix s = Matrix.translate(5, 6, 7);
+        Matrix s = Generator.translate(5, 6, 7);
         System.out.println(s);
 
 }
