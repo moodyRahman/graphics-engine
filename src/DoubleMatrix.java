dd12c0a15a836eb707de229b8ea07046b04a339b
Moody Rahman <mrahman01@stuy.edu>
2020-03-23 13:01:51 -0400

    formatting

--- src/DoubleMatrix.java
+++ src/DoubleMatrix.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.util.ArrayList;
 import java.util.ArrayDeque;
 import java.util.Iterator;
@@ -8,229 +9,222 @@ import java.util.Iterator;
  */
 public class DoubleMatrix {
 
+	private ArrayDeque<Double[]> matrix;
+	private int cols;
+	private int rows = 4;
 
+	/**
+	 * Initializes a new DoubleMatrix with 0 columns
+	 */
+	public DoubleMatrix() {
+		this.matrix = new ArrayDeque<Double[]>();
+		this.cols = 0;
+	}
 
-private ArrayDeque<Double[]> matrix;
-private int cols;
-private int rows = 4;
+	/**
+	 * Initializes a new DoubleMatrix with an input 2D array
+	 * 
+	 * @param in 2D array to be inputted
+	 */
+	public DoubleMatrix(double[][] in) {
+		this.matrix = new ArrayDeque<Double[]>();
 
-/**
- * Initializes a new DoubleMatrix with 0 columns
- */
-public DoubleMatrix(){
-	this.matrix = new ArrayDeque<Double[]>();
-	this.cols = 0;
-}
+		for (int x = 0; x < in.length; x++) {
+			Double[] toadd = new Double[4];
+			for (int y = 0; y < in[x].length; y++) {
+				toadd[y] = in[x][y];
+			}
+			matrix.add(toadd);
+		}
+		this.cols = in.length;
+	}
 
-/**
- * Initializes a new DoubleMatrix with an input 2D array
- * @param in 2D array to be inputted
- */
-public DoubleMatrix(double[][] in){
-	this.matrix = new ArrayDeque<Double[]>();
+	public Iterator<Double[]> getiterator() {
+		return this.matrix.iterator();
+	}
 
-	for (int x = 0; x < in.length; x++) {
+	/**
+	 * Wipes the current matrix and deletes all columns
+	 */
+	public void wipe() {
+		this.matrix.clear();
+	}
+
+	/**
+	 * returns how many columns (podoubles) are in the matrix
+	 * 
+	 * @return how many columsn there are
+	 */
+	public int getcols() {
+		return this.cols;
+	}
+
+	/**
+	 * returns how many rows there are in the matrix
+	 * 
+	 * @return how many rows there are
+	 */
+	public int getrows() {
+		return this.rows;
+	}
+
+	/**
+	 * Adds a new column (edge) to the matrix
+	 * 
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @param z z coordinate
+	 */
+	public void addpoint(double x, double y, double z) {
 		Double[] toadd = new Double[4];
-		for (int y = 0; y < in[x].length; y++) {
-			toadd[y] = in[x][y];
-		}
-		matrix.add(toadd);
-	}
-	this.cols = in.length;
-}
-
-
-public Iterator<Double[]> getiterator(){
-	return this.matrix.iterator();
-}
-
-/** 
- * Wipes the current matrix and deletes all columns
-*/
-public void wipe(){
-	this.matrix.clear();
-}
-
-
-/**
- * returns how many columns (podoubles) are in the matrix
- * @return how many columsn there are
- */
-public int getcols(){
-	return this.cols;
-}
-
-/**
- * returns how many rows there are in the matrix
- * @return how many rows there are
- */
-public int getrows(){
-	return this.rows;
-}
-
-
-/**
- * Adds a new column (edge) to the matrix
- * @param x x coordinate
- * @param y y coordinate
- * @param z z coordinate
- */
-public void addpoint(double x, double y, double z){
-	Double[] toadd = new Double[4];
-	toadd[0] = x;
-	toadd[1] = y;
-	toadd[2] = z;
-	toadd[3] = 1.0;
-	this.matrix.add(toadd);
-	this.cols++;
-}
-
-
-/**
- * Adjoins a matrix to this instance of matrix
- * @param append
- */
-public void addmatrixedge(DoubleMatrix append){
-	this.matrix.addAll(append.getinnermatrix());
-	cols+=append.getcols();
+		toadd[0] = x;
+		toadd[1] = y;
+		toadd[2] = z;
+		toadd[3] = 1.0;
+		this.matrix.add(toadd);
+		this.cols++;
 	}
 
-
-private ArrayDeque<Double[]> getinnermatrix(){
-	return this.matrix;
-}
-
-
-
-/**
- * add an adge from two points to the matrix
- * @param x0
- * @param y0
- * @param z0
- * @param x1
- * @param y1
- * @param z1
- */
-public void addedge(double x0, double y0, double z0, double x1, double y1, double z1){
-	this.addpoint(x0, y0, z0);
-	this.addpoint(x1, y1, z1);
-	this.cols+=2;
-}
-
-
-public void addpolygon(double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2, double z2){
-	this.addpoint(x0, y0, z0);
-	this.addpoint(x1, y1, z1);
-	this.addpoint(x2, y2, z2);
-	this.cols+=3;
-}
-
-
-/**
- * Graphical representation of the matrix
- * @return returns a String of the matrix, formatted nicely
- */
-public String toString(){
-	String out = "";
-	out += this.getcols();
-	return out;
-}
-
-
-
-/**
- * converts the internal array for the matrix into an double[][]
- * @return the doubleernal array as an double[][]
- */
-public double[][] getarray(){
-	double[][] out = new double[this.cols][this.rows];
-	Iterator<Double[]> outiter = this.matrix.iterator();
-	for (int x = 0; outiter.hasNext(); x++){
-		Double[] row = outiter.next();
-		for (int y = 0; y < this.rows; y++) {
-			out[x][y] = row[y];
-		}
+	/**
+	 * Adjoins a matrix to this instance of matrix
+	 * 
+	 * @param append
+	 */
+	public void addmatrixedge(DoubleMatrix append) {
+		this.matrix.addAll(append.getinnermatrix());
+		cols += append.getcols();
 	}
-	return out;
-}
 
-
-/**
- * multiplies two matrices
- * @param  a                    matrix a
- * @param  b                    matrix b
- * @return                      returns a new DoubleMatrix object with the result
- * @throws DoubleMatrixDimensionError mismatching dimensions for a and b
- */
-public static DoubleMatrix multiply (DoubleMatrix a, DoubleMatrix b) throws DoubleMatrixDimensionError {
-	double[][] out = new double[b.getcols()][a.getrows()];
-	double[][] ar = a.getarray();
-	double[][] br = b.getarray();
-	if (a.getcols() != b.getrows()) {
-		throw new DoubleMatrixDimensionError("mooos");
+	private ArrayDeque<Double[]> getinnermatrix() {
+		return this.matrix;
 	}
-	for (int x = 0; x < b.getcols(); x++) {
-		for (int y = 0; y < a.getrows(); y++) {
-			double cumsum = 0;
-			double[] ber = br[x];
-			ArrayList<Double> aer= new ArrayList<Double>();
-			for (int xer = 0; xer < ar.length; xer++) {
-				aer.add(ar[xer][y]);
+
+	/**
+	 * add an adge from two points to the matrix
+	 * 
+	 * @param x0
+	 * @param y0
+	 * @param z0
+	 * @param x1
+	 * @param y1
+	 * @param z1
+	 */
+	public void addedge(double x0, double y0, double z0, double x1, double y1, double z1) {
+		this.addpoint(x0, y0, z0);
+		this.addpoint(x1, y1, z1);
+		this.cols += 2;
+	}
+
+	public void addpolygon(double x0, double y0, double z0, double x1, double y1, double z1, double x2, double y2,
+			double z2) {
+		this.addpoint(x0, y0, z0);
+		this.addpoint(x1, y1, z1);
+		this.addpoint(x2, y2, z2);
+		this.cols += 3;
+	}
+
+	/**
+	 * Graphical representation of the matrix
+	 * 
+	 * @return returns a String of the matrix, formatted nicely
+	 */
+	public String toString() {
+		String out = "";
+		out += this.getcols();
+		return out;
+	}
+
+	/**
+	 * converts the internal array for the matrix into an double[][]
+	 * 
+	 * @return the doubleernal array as an double[][]
+	 */
+	public double[][] getarray() {
+		double[][] out = new double[this.cols][this.rows];
+		Iterator<Double[]> outiter = this.matrix.iterator();
+		for (int x = 0; outiter.hasNext(); x++) {
+			Double[] row = outiter.next();
+			for (int y = 0; y < this.rows; y++) {
+				out[x][y] = row[y];
 			}
-			ArrayList<Double> toprdoubleber = new ArrayList<Double>();
-			for (int d = 0; d < ber.length; d++) {
-				toprdoubleber.add(ber[d]);
-			}
-
-			for (int g = 0; g < ber.length; g++) {
-				cumsum += aer.get(g)*ber[g];
-			}
-
-			out[x][y] = cumsum;
-
 		}
+		return out;
 	}
 
-	// System.out.prdoubleln(new DoubleMatrix(out));
-	return new DoubleMatrix(out);
-}
+	/**
+	 * multiplies two matrices
+	 * 
+	 * @param a matrix a
+	 * @param b matrix b
+	 * @return returns a new DoubleMatrix object with the result
+	 * @throws DoubleMatrixDimensionError mismatching dimensions for a and b
+	 */
+	public static DoubleMatrix multiply(DoubleMatrix a, DoubleMatrix b) throws DoubleMatrixDimensionError {
+		double[][] out = new double[b.getcols()][a.getrows()];
+		double[][] ar = a.getarray();
+		double[][] br = b.getarray();
+		if (a.getcols() != b.getrows()) {
+			throw new DoubleMatrixDimensionError("mooos");
+		}
+		for (int x = 0; x < b.getcols(); x++) {
+			for (int y = 0; y < a.getrows(); y++) {
+				double cumsum = 0;
+				double[] ber = br[x];
+				ArrayList<Double> aer = new ArrayList<Double>();
+				for (int xer = 0; xer < ar.length; xer++) {
+					aer.add(ar[xer][y]);
+				}
+				ArrayList<Double> toprdoubleber = new ArrayList<Double>();
+				for (int d = 0; d < ber.length; d++) {
+					toprdoubleber.add(ber[d]);
+				}
 
-/**
- * Flushes the current matrix into an image file
- * @return new Image with the drawn matrix
- */
-public Image flushToImage(int width, int height, Pixel init, Pixel linecolor){
-	Image out = new Image(width, height, init);
+				for (int g = 0; g < ber.length; g++) {
+					cumsum += aer.get(g) * ber[g];
+				}
 
-	// Pixel c = new Pixel(0, 0, 0);
+				out[x][y] = cumsum;
 
-	// System.out.println("HERE");
-	System.out.println(matrix.size());
-	// for (int x = 0; x < matrix.size(); x+=2) {
-	// 	// System.out.println(x);
+			}
+		}
+
+		// System.out.prdoubleln(new DoubleMatrix(out));
+		return new DoubleMatrix(out);
+	}
+
+	/**
+	 * Flushes the current matrix into an image file
+	 * 
+	 * @return new Image with the drawn matrix
+	 */
+	public Image flushToImage(int width, int height, Pixel init, Pixel linecolor) {
+		Image out = new Image(width, height, init);
+
+		// Pixel c = new Pixel(0, 0, 0);
+
+		// System.out.println("HERE");
+		System.out.println(matrix.size());
+		// for (int x = 0; x < matrix.size(); x+=2) {
+		// // System.out.println(x);
 		// Double[] p1 = matrix.get(x);
 		// Double[] p2 = matrix.get(x+1);
-		
+
 		// out.line(p1[0], p1[1], p2[0], p2[1], linecolor);
-	// }
+		// }
 
-	Iterator<Double []> outiter = this.matrix.iterator();
+		Iterator<Double[]> outiter = this.matrix.iterator();
 
-	while (outiter.hasNext()){
-		Double[] p1 = outiter.next();
-		Double[] p2 = outiter.next();
-		out.line(p1[0], p1[1], p2[0], p2[1], linecolor);
+		while (outiter.hasNext()) {
+			Double[] p1 = outiter.next();
+			Double[] p2 = outiter.next();
+			out.line(p1[0], p1[1], p2[0], p2[1], linecolor);
+		}
+
+		return out;
 	}
 
-	return out;
-}
+	public static void main(String[] args) {
 
-
-public static void main(String[] args) {
-
+	}
 
 }
-
-}
-
-
