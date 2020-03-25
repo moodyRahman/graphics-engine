81fee677f676f6f807012551a29bd36eb06f10c1
Moody Rahman <mrahman01@stuy.edu>
2020-03-24 20:17:08 -0400

    Point class to organize polygons

---
+++ src/Point.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Point{
+        public double x;
+        public double y;
+        public double z;
+        public int id;
+        public Point(double x, double y, double z, int id){
+                this.x = x;
+                this.y = y;
+                this.z = z;
+                this.id = id;
+        }
+}
