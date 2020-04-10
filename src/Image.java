422339daf2ab80745f8e8aa9a192cb0fe1ad4c58
Moody Rahman <mrahman01@stuy.edu>
2020-04-10 18:44:01 -0400

    removed comments

--- src/Image.java
+++ src/Image.java
@@ -308,11 +308,6 @@ public void matrixLinePolygon(DoubleMatrix m, Pixel c){
 			line(p1[0], p1[1], p2[0], p2[1], c);
 			line(p2[0], p2[1], p0[0], p0[1], c);
 		}
-
-		// line(p0[0], p0[1], p1[0], p1[1], c);
-		// line(p1[0], p1[1], p2[0], p2[1], c);
-		// line(p2[0], p2[1], p0[0], p0[1], c);
-
 	}
 
 }
